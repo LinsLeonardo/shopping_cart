798329996464d4d872276641478c4256d3a66dc1
LinsLeonardo <linsleonardo22@gmail.com>
2021-11-18 16:35:35 -0300

    novos estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingCartContainerComponent } from './shopping-cart-container/shopping-cart-container.component';
+import { ItemsComponent } from './shopping-cart-container/items/items.component';
+import { HeaderComponent } from './shopping-cart-container/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartContainerComponent,
+    ItemsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
